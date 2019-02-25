ad8e2a7ac8139b1d5be69ff6ad0ed1b6dd762a9f
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-25 08:00:19 -0800

    getting the button to animate the square

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   animations: [
     trigger('divState', [
-       state('normal', style({
-         'background-color': 'red',
-         transform: 'translateX(0)'
-       })),
-       state('highlighted', style({
-        'background-color': 'blue',
-        transform: 'translateX(100px)'
-       }))
+        state('normal', style({
+          'background-color': 'red',
+          transform: 'translateX(0)'
+        })),
+        state('highlighted', style({
+          'background-color': 'blue',
+          transform: 'translateX(100px)'
+        })),
+        transition('normal => highlighted', animate(300)),
+        transition('highlighted => normal', animate(800)),
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
+
+    onAnimate() {
+      this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+    }
 
     onAdd(item) {
       this.list.push(item);
