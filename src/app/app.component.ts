8130ff395c17f9ce121f31f1adb9ec5c1eaced07
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-25 08:05:21 -0800

    more animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,20 +14,43 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
           'background-color': 'blue',
           transform: 'translateX(100px)'
         })),
-        transition('normal => highlighted', animate(300)),
-        transition('highlighted => normal', animate(800)),
-    ])
+        transition('normal <=> highlighted', animate(300)),
+        // transition('highlighted => normal', animate(800)),
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500)),
+  ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
-    onAnimate() {
-      this.state = this.state === 'normal' ? 'highlighted' : 'normal';
-    }
+  onAnimate() {
+    this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+    this.wildState = this.state === 'normal' ? 'highlighted' : 'normal';
+  }
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
 }
